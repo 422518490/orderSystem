978cca141c480d8ba6fd020849fe4bacb986240e
liaoyubo <357029540@qq.com>
2018-11-23 17:26:16 +0800

    新增gateway模块

--- project/一期/orderSystem/common/src/main/java/com/yaya/common/util/ExcelUtils.java
+++ project/一期/orderSystem/common/src/main/java/com/yaya/common/util/ExcelUtils.java
@@ -1,23 +1,19 @@
 package com.yaya.common.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.web.multipart.MultipartFile;
-
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author liaoyubo
@@ -26,67 +22,68 @@ import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
  */
 public class ExcelUtils {
 
-	public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName,
-			boolean isCreateHeader, HttpServletResponse response) throws Exception {
-		ExportParams exportParams = new ExportParams(title, sheetName);
-		exportParams.setCreateHeadRows(isCreateHeader);
-		defaultExport(list, pojoClass, fileName, response, exportParams);
+    public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName,
+                                   boolean isCreateHeader, HttpServletResponse response) throws Exception {
+        ExportParams exportParams = new ExportParams(title, sheetName);
+        exportParams.setCreateHeadRows(isCreateHeader);
+        defaultExport(list, pojoClass, fileName, response, exportParams);
 
-	}
-	public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName,
-			HttpServletResponse response) throws Exception {
-		defaultExport(list, pojoClass, fileName, response, new ExportParams(title, sheetName));
-	}
+    }
 
-	public static void exportExcel(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws Exception {
-		defaultExport(list, fileName, response);
-	}
+    public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName,
+                                   HttpServletResponse response) throws Exception {
+        defaultExport(list, pojoClass, fileName, response, new ExportParams(title, sheetName));
+    }
 
-	private static void defaultExport(List<?> list, Class<?> pojoClass, String fileName, HttpServletResponse response,
-			ExportParams exportParams) throws Exception {
-		Workbook workbook = ExcelExportUtil.exportExcel(exportParams, pojoClass, list);
-		if (workbook != null){
-			downLoadExcel(fileName, response, workbook);
-		}
-	}
+    public static void exportExcel(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws Exception {
+        defaultExport(list, fileName, response);
+    }
 
-	private static void defaultExport(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws Exception {
-		Workbook workbook = ExcelExportUtil.exportExcel(list, ExcelType.HSSF);
-		if (workbook != null){
-			downLoadExcel(fileName, response, workbook);
-		}
-	}
+    private static void defaultExport(List<?> list, Class<?> pojoClass, String fileName, HttpServletResponse response,
+                                      ExportParams exportParams) throws Exception {
+        Workbook workbook = ExcelExportUtil.exportExcel(exportParams, pojoClass, list);
+        if (workbook != null) {
+            downLoadExcel(fileName, response, workbook);
+        }
+    }
 
-	public static void downLoadExcel(String fileName, HttpServletResponse response, Workbook workbook) throws Exception{
-		response.setCharacterEncoding("UTF-8");
-		response.setHeader("content-Type", "application/vnd.ms-excel");
-		response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
-		workbook.write(response.getOutputStream());
-	}
+    private static void defaultExport(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws Exception {
+        Workbook workbook = ExcelExportUtil.exportExcel(list, ExcelType.HSSF);
+        if (workbook != null) {
+            downLoadExcel(fileName, response, workbook);
+        }
+    }
 
-	public static <T> List<T> importExcel(String filePath, Integer titleRows, Integer headerRows, Class<T> pojoClass) {
-		if (StringUtils.isBlank(filePath)) {
-			return null;
-		}
-		ImportParams params = new ImportParams();
-		params.setTitleRows(titleRows);
-		params.setHeadRows(headerRows);
-		List<T> list = null;
-		list = ExcelImportUtil.importExcel(new File(filePath), pojoClass, params);
-		return list;
-	}
+    public static void downLoadExcel(String fileName, HttpServletResponse response, Workbook workbook) throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("content-Type", "application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+        workbook.write(response.getOutputStream());
+    }
 
-	public static <T> List<T> importExcel(MultipartFile file, Integer titleRows, Integer headerRows,
-			Class<T> pojoClass) throws Exception {
-		if (file == null) {
-			return null;
-		}
-		ImportParams params = new ImportParams();
-		params.setTitleRows(titleRows);
-		params.setHeadRows(headerRows);
-		List<T> list = null;
-		list = ExcelImportUtil.importExcel(file.getInputStream(), pojoClass, params);
-		return list;
-	}
+    public static <T> List<T> importExcel(String filePath, Integer titleRows, Integer headerRows, Class<T> pojoClass) {
+        if (StringUtils.isBlank(filePath)) {
+            return null;
+        }
+        ImportParams params = new ImportParams();
+        params.setTitleRows(titleRows);
+        params.setHeadRows(headerRows);
+        List<T> list = null;
+        list = ExcelImportUtil.importExcel(new File(filePath), pojoClass, params);
+        return list;
+    }
+
+    public static <T> List<T> importExcel(MultipartFile file, Integer titleRows, Integer headerRows,
+                                          Class<T> pojoClass) throws Exception {
+        if (file == null) {
+            return null;
+        }
+        ImportParams params = new ImportParams();
+        params.setTitleRows(titleRows);
+        params.setHeadRows(headerRows);
+        List<T> list = null;
+        list = ExcelImportUtil.importExcel(file.getInputStream(), pojoClass, params);
+        return list;
+    }
 
 }
---
+++ project/一期/orderSystem/gateway/src/main/java/com/yaya/gateway/GatewayApp.java
@@ -0,0 +1,23 @@
+package com.yaya.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author liaoyubo
+ * @version 1.0
+ * @date 2018/11/23
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+public class GatewayApp {
+
+    public static void main(String [] args){
+        SpringApplication.run(GatewayApp.class,args);
+    }
+}
