babece66f41d282b35a54efae2e3411a8b6be053
liaoyubo <357029540@qq.com>
2019-03-08 15:12:27 +0800

    添加alibaba相关cloud的注册中心组件

--- project/一期/orderSystem/merchant/src/main/java/com/yaya/merchant/MerchantApp.java
+++ project/一期/orderSystem/merchant/src/main/java/com/yaya/merchant/MerchantApp.java
@@ -3,12 +3,7 @@ package com.yaya.merchant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.refresh.ContextRefresher;
-import org.springframework.cloud.context.scope.refresh.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author liaoyubo
@@ -16,13 +11,12 @@ import org.springframework.context.annotation.Bean;
  * @description 商家启动类
  */
 @SpringBootApplication(scanBasePackages = "com.yaya")
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 public class MerchantApp {
 
-    public static void main(String [] args){
-        SpringApplication.run(MerchantApp.class,args);
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApp.class, args);
     }
 /*
     @Bean
--- project/一期/orderSystem/nacosServer/src/main/java/com/yaya/nacoskaserver/NacosServerApp.java
+++ project/一期/orderSystem/nacosServer/src/main/java/com/yaya/nacoskaserver/NacosServerApp.java
@@ -1,10 +1,10 @@
-package com.yaya.eurekaServer;
+package com.yaya.nacoskaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liaoyubo
@@ -12,12 +12,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @description
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-public class EurekaServerApp {
+public class NacosServerApp {
 
-    public static void main(String [] args){
-        SpringApplication.run(EurekaServerApp.class,args);
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApp.class, args);
     }
 
 }
