187b8f4b7f0c7807d24c34b9cb8255504dedbfc1
liaoyubo <357029540@qq.com>
2019-03-18 16:21:58 +0800

    测试配置中心

--- project/一期/orderSystem/merchant/src/main/java/com/yaya/merchant/controller/CloudConfigServerController.java
+++ project/一期/orderSystem/merchant/src/main/java/com/yaya/merchant/controller/CloudConfigServerController.java
@@ -1,6 +1,7 @@
 package com.yaya.merchant.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RefreshScope
 public class CloudConfigServerController {
     @Value("${test_name}")
     private String name;
