9de8c82af97d7ab4729b587596911f047816e185
liaoyubo <357029540@qq.com>
2019-12-11 10:50:33 +0800

    修改DateUtils工具类

---
+++ project/一期/orderSystem/common/src/main/java/com/yaya/common/constant/ErrorRespCode.java
@@ -0,0 +1,22 @@
+package com.yaya.common.constant;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * @author liaoyubo
+ * @version 1.0
+ * @date 2018/5/23
+ * @description 错误参数返回静态使用类
+ */
+public class ErrorRespCode {
+
+    public static final int INTEGER_NULL_ERROR = -999999999;
+
+    public static final Date DATE_NULL_ERROR = null;
+
+    public static final String STRING_NULL_ERROR = "";
+
+    public static final LocalDate LOCALDATE_NULL_ERROR = null;
+
+}
--- project/一期/orderSystem/common/src/main/java/com/yaya/common/util/DateUtils.java
+++ project/一期/orderSystem/common/src/main/java/com/yaya/common/util/DateUtils.java
@@ -1,18 +1,24 @@
 package com.yaya.common.util;
 
+
+import com.yaya.common.constant.ErrorRespCode;
+import lombok.extern.slf4j.Slf4j;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Optional;
 
 /**
  * @author liaoyubo
- * @version 1.0 2018/3/22
- * @description 日期工具类
+ * @version 1.0
+ * @date 2019/5/22
+ * @description
  */
+@Slf4j
 public class DateUtils {
 
     /**
@@ -30,17 +36,29 @@ public class DateUtils {
      */
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
+    public static final String DATETIME_FORMAT_NO_SYMBOL = "yyyyMMddHHmmss";
+
+    public static final String DATE_FORMAT_NO_SYMBOL = "yyyyMMdd";
+
+    public static final String DATETIME_FORMAT_T = "yyyy-MM-ddTHH:mm:ss";
+
     /**
      * 判断字符串是否为指定的日期格式
      *
      * @param dateStr 需要判断的字符串
-     * @param format 指定的日期格式
+     * @param format  指定的日期格式
      * @return true=字符串是日期格式
      */
     public static boolean isDateFormat(String dateStr, String format) {
         try {
+            //判断日期字符串和日期格式是否为空
+            if (!Optional.ofNullable(dateStr).isPresent() || "".equals(dateStr)
+                    || !Optional.ofNullable(format).isPresent() || "".equals(format)) {
+                return false;
+            }
             new SimpleDateFormat(format).parse(dateStr);
         } catch (ParseException e) {
+            log.error("判断字符串是否为指定的日期格式解析错误:" + e);
             return false;
         }
         return true;
@@ -50,23 +68,34 @@ public class DateUtils {
      * 将Date时间转成字符串
      */
     public static String dateToStr(Date date, String format) {
-        if(null == date){
-            return "";
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(date).isPresent()
+                || !Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.STRING_NULL_ERROR;
         }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
         return simpleDateFormat.format(date);
     }
 
     /**
-     * 将字符串时间改成Date类型
+     * 将Date改成指定Date类型
      */
     public static Date formatDate(Date date, String format) {
-        Date newDate;
+        Date newDate = null;
         try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-            newDate = simpleDateFormat.parse(dateToStr(date, format));
+            //判断日期格式是否为空
+            if (!Optional.ofNullable(format).isPresent() || "".equals(format)) {
+                newDate = ErrorRespCode.DATE_NULL_ERROR;
+            }
+            //判断日期字符串是否为空,为空则取当前日期
+            if (!Optional.ofNullable(date).isPresent()) {
+                newDate = now(format);
+            } else {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+                newDate = simpleDateFormat.parse(dateToStr(date, format));
+            }
         } catch (ParseException e) {
-            newDate = now(format);
+            log.error("将Date改成指定Date类型解析错误:" + e);
             e.printStackTrace();
         }
         return newDate;
@@ -75,26 +104,32 @@ public class DateUtils {
     /**
      * 将字符串时间改成Date类型
      */
-    public static Date strToDate(String dateStr, String format) {
-        Date date = null;
-        try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-            date = simpleDateFormat.parse(dateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
+    public static Date strToDate(String dateStr, String format) throws ParseException {
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(dateStr).isPresent() || "".equals(dateStr)
+                || !Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.DATE_NULL_ERROR;
         }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        Date date = simpleDateFormat.parse(dateStr);
         return date;
     }
 
     /**
-     * 将字符串时间改成Date类型
+     * 将字符串时间改成指定格式化类型
      */
     public static String toStringDate(String dateStr, String format) {
-        String result = dateStr;
+        String result = "";
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(dateStr).isPresent() || "".equals(dateStr)
+                || !Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
         try {
             Date date = new SimpleDateFormat(format).parse(dateStr);
             result = new SimpleDateFormat(format).format(date);
         } catch (ParseException e) {
+            log.error("将字符串时间改成指定格式化类型解析错误:" + e);
             e.printStackTrace();
         }
         return result;
@@ -108,7 +143,13 @@ public class DateUtils {
      * @return
      */
     public static int getTimeDelta(Date date1, Date date2) {
-        long timeDelta = (date1.getTime() - date2.getTime()) / 1000; //单位是秒
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(date1).isPresent()
+                || !Optional.ofNullable(date2).isPresent()) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
+        //单位是秒
+        long timeDelta = (date1.getTime() - date2.getTime()) / 1000;
         int secondsDelta = timeDelta > 0 ? (int) timeDelta : (int) Math.abs(timeDelta);
         return secondsDelta;
     }
@@ -120,6 +161,10 @@ public class DateUtils {
      * @return
      */
     public static int getTimeDelta(Date date) {
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
         return getTimeDelta(new Date(), date);
     }
 
@@ -130,6 +175,10 @@ public class DateUtils {
      * @return
      */
     public static String getTimeDeltaToString(Date date) {
+        //判断日期字符串和日期格式是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
         int diff = getTimeDelta(date);
         long days = diff / (60 * 60 * 24);
         long hours = (diff - days * (60 * 60 * 24)) / (60 * 60);
@@ -137,7 +186,7 @@ public class DateUtils {
         long seconds = (diff - days * (60 * 60 * 24) - hours * (60 * 60) - minutes * 60);
         StringBuilder result = new StringBuilder();
         if (days > 0) {
-            result.append(date).append("天");
+            result.append(days).append("天");
         }
         if (hours > 0) {
             result.append(hours).append("小时");
@@ -166,7 +215,11 @@ public class DateUtils {
      * @param format 时间格式
      * @return Date
      */
-    public static Date now(String format) {
+    public static Date now(String format) throws ParseException {
+        //判断日期格式是否为空
+        if (!Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         SimpleDateFormat formatter = new SimpleDateFormat(format);
         return strToDate(formatter.format(now()), format);
     }
@@ -178,6 +231,10 @@ public class DateUtils {
      * @return 0=星期天
      */
     public static int getWeekCodeOfDate(Date dt) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(dt).isPresent()) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
         Calendar cal = Calendar.getInstance();
         cal.setTime(dt);
         int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
@@ -191,6 +248,10 @@ public class DateUtils {
      * @return
      */
     public static String getWeekOfDate(Date dt) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(dt).isPresent()) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
         String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
         return weekDays[getWeekCodeOfDate(dt)];
     }
@@ -202,6 +263,26 @@ public class DateUtils {
      * @return 指定时间与当前时间之间相差的天数
      */
     public static long betweenDays(Date start) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(start).isPresent()) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
+        LocalDate startDateInclusive = dateToLocalDate(start);
+        return LocalDate.now().toEpochDay() - startDateInclusive.toEpochDay();
+    }
+
+    /**
+     * 得到指定时间与当前时间之间相差的天数
+     *
+     * @param startDate 指定时间
+     * @return 指定时间与当前时间之间相差的天数
+     */
+    public static long betweenDays(String startDate) throws ParseException {
+        //判断日期是否为空
+        if (startDate == null || "".equals(startDate)) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
+        Date start = strToDate(startDate, "yyyy-MM-dd");
         LocalDate startDateInclusive = dateToLocalDate(start);
         return LocalDate.now().toEpochDay() - startDateInclusive.toEpochDay();
     }
@@ -210,10 +291,15 @@ public class DateUtils {
      * 得到两个之间之间相差的天数
      *
      * @param start 开始时间
-     * @param end 结束时间
+     * @param end   结束时间
      * @return 开始时间与结束时间相差的天数
      */
     public static long betweenDays(Date start, Date end) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(start).isPresent()
+                || !Optional.ofNullable(end).isPresent()) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
         LocalDate startDateInclusive = dateToLocalDate(start);
         LocalDate endDateExclusive = dateToLocalDate(end);
         return endDateExclusive.toEpochDay() - startDateInclusive.toEpochDay();
@@ -223,29 +309,42 @@ public class DateUtils {
      * 得到两个之间之间相差的天数
      *
      * @param startDate 开始时间
-     * @param endDate 结束时间
+     * @param endDate   结束时间
      * @return 开始时间与结束时间相差的天数
      */
-    public static long betweenDays(String startDate, String endDate) {
-        Date start = DateUtils.strToDate(startDate, "yyyy-MM-dd");
-        Date end = DateUtils.strToDate(endDate, "yyyy-MM-dd");
+    public static long betweenDays(String startDate, String endDate) throws ParseException {
+        //判断日期是否为空
+        if (!Optional.ofNullable(startDate).isPresent() || "".equals(startDate)
+                || !Optional.ofNullable(endDate).isPresent() || "".equals(endDate)) {
+            return ErrorRespCode.INTEGER_NULL_ERROR;
+        }
+        Date start = strToDate(startDate, "yyyy-MM-dd");
+        Date end = strToDate(endDate, "yyyy-MM-dd");
         LocalDate startDateInclusive = dateToLocalDate(start);
         LocalDate endDateExclusive = dateToLocalDate(end);
         return endDateExclusive.toEpochDay() - startDateInclusive.toEpochDay();
     }
 
     public static LocalDate dateToLocalDate(Date date) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.LOCALDATE_NULL_ERROR;
+        }
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalDate();
     }
 
     /**
      * 获取在参考时间的基础上增加或者减少秒数后的时间
      *
-     * @param date 参考时间
+     * @param date         参考时间
      * @param secondAmount 秒数，如果是减少秒数传一个负数
      * @return 格式为 yyyy-MM-dd HH:mm:ss 的新时间
      */
     public static Date getDateBySecond(Date date, int secondAmount) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.SECOND, secondAmount);
@@ -264,12 +363,18 @@ public class DateUtils {
 
     /**
      * 判断时间是否在时间段内
+     *
      * @param nowTime
      * @param beginTime
      * @param endTime
      * @return
      */
     public static boolean belongCalendar(Date nowTime, Date beginTime, Date endTime) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(nowTime).isPresent() || !Optional.ofNullable(beginTime).isPresent()
+                || !Optional.ofNullable(endTime).isPresent()) {
+            return false;
+        }
         Calendar date = Calendar.getInstance();
         date.setTime(nowTime);
 
@@ -286,10 +391,14 @@ public class DateUtils {
      * 在指定日期上加指定的天数
      *
      * @param date 基础日期
-     * @param day 天数
+     * @param day  天数
      * @return 增加天数后的日期
      */
     public static Date addDay(Date date, int day) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DAY_OF_MONTH, day);
@@ -299,11 +408,15 @@ public class DateUtils {
     /**
      * 在指定日期上加指定的分钟数
      *
-     * @param date 基础日期
+     * @param date   基础日期
      * @param minute 分钟数
      * @return 增加分钟数后的日期
      */
     public static Date addMinute(Date date, int minute) {
+        //判断日期是否为空
+        if (!Optional.ofNullable(date).isPresent()) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.MINUTE, minute);
@@ -312,14 +425,19 @@ public class DateUtils {
 
     /**
      * 将时间转换为时间戳
-     * @param s
+     *
+     * @param dateStr
      * @return
      * @throws ParseException
      */
-    public static String dateToStamp(String s) throws ParseException{
+    public static String dateToStamp(String dateStr) throws ParseException {
+        //判断日期字符串是否为空
+        if (!Optional.ofNullable(dateStr).isPresent() || "".equals(dateStr)) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = simpleDateFormat.parse(s);
+        Date date = simpleDateFormat.parse(dateStr);
         long ts = date.getTime();
         res = String.valueOf(ts);
         return res;
@@ -332,6 +450,10 @@ public class DateUtils {
      * @return 日期格式的时间
      */
     public static Date stampToDate(Long time) {
+        //判断时间戳是否为空
+        if (!Optional.ofNullable(time).isPresent()) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         return stampToDate(time, DEFAULT_DATETIME_FORMAT);
     }
 
@@ -342,6 +464,11 @@ public class DateUtils {
      * @return 日期格式的时间
      */
     public static Date stampToDate(Long time, String format) {
+        //判断时间戳和日期格式是否为空
+        if (!Optional.ofNullable(time).isPresent()
+                || !Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.DATE_NULL_ERROR;
+        }
         SimpleDateFormat dateFormat = new SimpleDateFormat(format);
         String d = dateFormat.format(time);
         Date date = null;
@@ -355,30 +482,93 @@ public class DateUtils {
 
     /**
      * 将时间戳转换为时间
-     * @param s
+     *
+     * @param timeStr
      * @return
      */
-    public static String stampToDate(String s){
-        String res;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        long lt = new Long(s);
-        Date date = new Date(lt);
-        res = simpleDateFormat.format(date);
-        return res;
+    public static String stampToDate(String timeStr) {
+        //判断时间戳是否为空
+        if (!Optional.ofNullable(timeStr).isPresent() || "".equals(timeStr)) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DEFAULT_DATETIME_FORMAT);
+        Date date = new Date(Long.parseLong(timeStr));
+        return simpleDateFormat.format(date);
     }
 
     /**
      * 获取当前日期的年月日
+     *
      * @return
      */
-    public static String getCurrentDate(String format)
-    {
-    	Date curdate = new Date();
-    	return dateToStr(curdate, format);
+    public static String getCurrentDate(String format) {
+        //判断日期格式是否为空
+        if (!Optional.ofNullable(format).isPresent() || "".equals(format)) {
+            return ErrorRespCode.STRING_NULL_ERROR;
+        }
+        Date curdate = new Date();
+        return dateToStr(curdate, format);
     }
 
-    public static void main(String[] args) {
-        Date date = DateUtils.strToDate("2017-11-01 10:12:19", DateUtils.DEFAULT_DATETIME_FORMAT);
-        System.out.println(DateUtils.getTimeDeltaToString(date));
+    public static long betweenSecond(Date beginDate, Date endDate) throws Exception {
+        if (!Optional.ofNullable(beginDate).isPresent()
+                || !Optional.ofNullable(endDate).isPresent()) {
+            throw new Exception("比较的时间不能为空");
+        }
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        Instant beginInstant = beginDate.toInstant();
+        LocalDateTime beginLocal = beginInstant.atZone(zoneId).toLocalDateTime();
+        Instant endInstant = endDate.toInstant();
+        LocalDateTime endLocal = endInstant.atZone(zoneId).toLocalDateTime();
+        return Duration.between(beginLocal, endLocal).getSeconds();
+    }
+
+    public static long betweenDays(int year, int month, int day) {
+        LocalDate today = LocalDate.now();
+        LocalDate localDate = LocalDate.of(year, month, day);
+        // 直接使用日期类中的方法计算日期间隔天数
+        long days = localDate.until(today, ChronoUnit.DAYS);
+        return days;
+    }
+
+    public static long betweenWeeks(int year, int month, int day) {
+        LocalDate today = LocalDate.now();
+        LocalDate localDate = LocalDate.of(year, month, day);
+        // 直接使用日期类中的方法计算日期间隔周数
+        long weeks = localDate.until(today, ChronoUnit.WEEKS);
+        return weeks;
+    }
+
+    public static long betweenMonths(int year, int month, int day) {
+        LocalDate today = LocalDate.now();
+        LocalDate localDate = LocalDate.of(year, month, day);
+        // 直接使用日期类中的方法计算日期间隔月数
+        long months = localDate.until(today, ChronoUnit.MONTHS);
+        return months;
+    }
+
+    public static long betweenYears(int year, int month, int day) {
+        LocalDate today = LocalDate.now();
+        LocalDate localDate = LocalDate.of(year, month, day);
+        // 直接使用日期类中的方法计算日期间隔年数
+        long years = localDate.until(today, ChronoUnit.YEARS);
+        return years;
+    }
+
+    public static void main(String[] args) throws Exception {
+        /*Date now = DateUtils.now();
+        Date timeoutBeginDate = DateUtils.getDateBySecond(now, 10 * -1);
+        String date = DateUtils.dateToStr(timeoutBeginDate, DateUtils.DEFAULT_DATETIME_FORMAT);
+        System.out.println(now);
+        System.out.println(date);*/
+        long days = betweenDays(2019, 6, 14);
+        System.out.println(days);
+        long days1 = betweenDays(2019, 6, 7);
+        System.out.println(days1);
+        long weeks = betweenWeeks(2019, 6, 8);
+        System.out.println(weeks);
+        long days2 = betweenDays(DateUtils.strToDate("2019-11-15 10:27:35", DateUtils.DEFAULT_DATETIME_FORMAT), DateUtils.now());
+        System.out.println(days2);
     }
 }
